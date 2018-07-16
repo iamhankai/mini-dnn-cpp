0522c93d28b756f315ae02025bec02f1a4ab95cf
hankai <hankai@hankaideMacBook-Pro.local>
2018-07-16 17:51:27 +0800

    add accuracy test

--- example.cc
+++ example.cc
@@ -6,8 +6,6 @@
 #include "src/mse_loss.h"
 #include "src/sigmoid.h"
 
-//using Eigen::MatrixXd;
-
 int main()
 {
   // data
@@ -15,8 +13,8 @@ int main()
   dataset.read();
   int n_train = dataset.train_data.cols();
   int dim_in = dataset.train_data.rows();
-  int batch_size = 32;
-  std::cout << "mnist: " << dataset.train_data.block(0, 2, 784, 4) << std::endl;
+  const int batch_size = 32;
+  std::cout << "mnist: " << n_train << std::endl;
   std::cout << dataset.test_labels.cols() << std::endl;
   // dnn
   FullyConnected fc1(dim_in, 128);
@@ -26,27 +24,39 @@ int main()
   // loss
   MSE mse;
   SGD opt(0.01, 0.0001);
-  for (int start_idx = 0; start_idx < n_train; start_idx += batch_size) {
-    Matrix x_batch = dataset.train_data.block(0, start_idx, dim_in, 
-                                      std::min(batch_size, n_train - start_idx));
-    Matrix label_batch = dataset.train_labels.block(0, start_idx, 1, 
-                                      std::min(batch_size, n_train - start_idx));
-    Matrix target_batch = one_hot_encode(label_batch, 10);
-    fc1.forward(x_batch);
+  const int n_epoch = 5;
+  for (int epoch = 0; epoch < n_epoch; epoch ++) {
+    for (int start_idx = 0; start_idx < n_train; start_idx += batch_size) {
+      Matrix x_batch = dataset.train_data.block(0, start_idx, dim_in, 
+                                        std::min(batch_size, n_train - start_idx));
+      Matrix label_batch = dataset.train_labels.block(0, start_idx, 1, 
+                                        std::min(batch_size, n_train - start_idx));
+      Matrix target_batch = one_hot_encode(label_batch, 10);
+      fc1.forward(x_batch);
+      sigmoid1.forward(fc1.output());
+      fc2.forward(sigmoid1.output());
+      sigmoid2.forward(fc2.output());
+      mse.evaluate(sigmoid2.output(), target_batch);
+      
+      int ith_batch = start_idx / batch_size;
+      if((ith_batch % 10) == 0){
+        std::cout << ith_batch << "-th batch, loss: " << mse.output() << std::endl;
+      }
+
+      sigmoid2.backward(fc2.output(), mse.back_gradient());
+      fc2.backward(sigmoid1.output(), sigmoid2.back_gradient());
+      sigmoid1.backward(fc1.output(), fc2.back_gradient());
+      fc1.backward(x_batch, sigmoid1.back_gradient());
+      fc2.update(opt);
+      fc1.update(opt);
+    }
+    // test
+    fc1.forward(dataset.test_data);
     sigmoid1.forward(fc1.output());
     fc2.forward(sigmoid1.output());
     sigmoid2.forward(fc2.output());
-    mse.evaluate(sigmoid2.output(), target_batch);
-    //std::cout << "out: " << fc2.output() << std::endl;
-    //std::cout << "target: " << target_batch << std::endl;
-    std::cout << "loss: " << mse.output() << std::endl;
-
-    sigmoid2.backward(fc2.output(), mse.back_gradient());
-    fc2.backward(sigmoid1.output(), sigmoid2.back_gradient());
-    sigmoid1.backward(fc1.output(), fc2.back_gradient());
-    fc1.backward(x_batch, sigmoid1.back_gradient());
-    fc2.update(opt);
-    fc1.update(opt);
+    float acc = compute_accuracy(sigmoid2.output(), dataset.test_labels);
+    std::cout << epoch + 1 << "-th epoch, test acc: " << acc << std::endl;
   }
   return 0;
 }
