12c1a428740276644bdecebe6b8bc50799e867a2
loc4atnt <dorakid153@gmail.com>
2023-12-19 20:57:39 +0700

    yeh

--- src/device/Util.cu
+++ src/device/Util.cu
@@ -1,5 +1,42 @@
 #include "Util.h"
 
+struct GpuTimer
+{
+    cudaEvent_t start;
+    cudaEvent_t stop;
+
+    GpuTimer()
+    {
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+    }
+
+    ~GpuTimer()
+    {
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    }
+
+    void Start()
+    {
+        cudaEventRecord(start, 0);
+        cudaEventSynchronize(start);
+    }
+
+    void Stop()
+    {
+        cudaEventRecord(stop, 0);
+    }
+
+    float Elapsed()
+    {
+        float elapsed;
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&elapsed, start, stop);
+        return elapsed;
+    }
+};
+
 void printDeviceInfo()
 {
 	cudaDeviceProp devProv;
