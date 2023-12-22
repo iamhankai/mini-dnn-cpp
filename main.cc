13f24d3c362cde54f3031ddb48283ca6a2f7dd11
loc4atnt <dorakid153@gmail.com>
2023-12-22 17:53:38 +0700

    test bug

--- main.cc
+++ main.cc
@@ -28,68 +28,10 @@
 
 #include "src/device/Util.h"
 
-// int main() {
-//   printDeviceInfo();
-
-//   // Load Fashion MNIST dataset
-//   MNIST dataset("../data/mnist/");
-//   dataset.read();
-
-//   // Build Lenet5 model
-//   Network dnn;
-//   Layer* conv1 = new Conv(1, 28, 28, 6, 5, 5, 1, 0, 0);
-//   Layer* pool1 = new MaxPooling(6, 24, 24, 2, 2, 2);
-//   Layer* conv2 = new Conv(6, 12, 12, 16, 5, 5, 1, 0, 0);
-//   Layer* pool2 = new MaxPooling(16, 8, 8, 2, 2, 2);
-//   Layer* fc3 = new FullyConnected(pool2->output_dim(), 120);
-//   Layer* fc4 = new FullyConnected(120, 84);
-//   Layer* fc5 = new FullyConnected(84, 10);
-//   Layer* relu1 = new ReLU;
-//   Layer* relu2 = new ReLU;
-//   Layer* relu3 = new ReLU;
-//   Layer* relu4 = new ReLU;
-//   Layer* softmax = new Softmax;
-//   dnn.add_layer(conv1);
-//   dnn.add_layer(relu1);
-//   dnn.add_layer(pool1);
-//   dnn.add_layer(conv2);
-//   dnn.add_layer(relu2);
-//   dnn.add_layer(pool2);
-//   dnn.add_layer(fc3);
-//   dnn.add_layer(relu3);
-//   dnn.add_layer(fc4);
-//   dnn.add_layer(relu4);
-//   dnn.add_layer(fc5);
-//   dnn.add_layer(softmax);
-
-//   // load parameters
-//   std::vector<float> conv1Parameters = loadParametersFromFile("../parameters/conv1.txt");
-//   std::vector<float> conv2Parameters = loadParametersFromFile("../parameters/conv2.txt");
-//   std::vector<float> fc3Parameters = loadParametersFromFile("../parameters/fc3.txt");
-//   std::vector<float> fc4Parameters = loadParametersFromFile("../parameters/fc4.txt");
-//   std::vector<float> fc5Parameters = loadParametersFromFile("../parameters/fc5.txt");
-//   std::cout << "conv1 paramters: " << conv1Parameters.size() << std::endl;
-//   conv1->set_parameters(conv1Parameters);
-//   std::cout << "conv2 paramters: " << conv2Parameters.size() << std::endl;
-//   conv2->set_parameters(conv2Parameters);
-//   std::cout << "fc3 paramters: " << fc3Parameters.size() << std::endl;
-//   fc3->set_parameters(fc3Parameters);
-//   std::cout << "fc4 paramters: " << fc4Parameters.size() << std::endl;
-//   fc4->set_parameters(fc4Parameters);
-//   std::cout << "fc5 paramters: " << fc5Parameters.size() << std::endl;
-//   fc5->set_parameters(fc5Parameters);
-
-//   // Test (Run forward)
-//   dnn.forward(dataset.test_data);
-//   float acc = compute_accuracy(dnn.output(), dataset.test_labels);
-//   std::cout << std::endl;
-//   std::cout << "Test acc: " << acc << std::endl;
-
-//   return 0;
-// }
-
-int main() {
+int main(int argc, char *argv[]) {
   printDeviceInfo();
+
+  bool isTraining = (argc > 1 && (strcmp(argv[1], "train") == 0));
 
   // Load Fashion MNIST dataset
   MNIST dataset("../data/mnist/");
@@ -97,23 +39,94 @@ int main() {
 
   // Build Lenet5 model
   Network dnn;
-  Layer* pool1 = new MaxPooling(1, 28, 28, 14, 14, 14);
-  Layer* fc2 = new FullyConnected(pool1->output_dim(), 10);
+  Layer* conv1 = new Conv(1, 28, 28, 6, 5, 5, 1, 0, 0);
+  Layer* pool1 = new MaxPooling(6, 24, 24, 2, 2, 2);
+  Layer* conv2 = new Conv(6, 12, 12, 16, 5, 5, 1, 0, 0);
+  Layer* pool2 = new MaxPooling(16, 8, 8, 2, 2, 2);
+  Layer* fc3 = new FullyConnected(pool2->output_dim(), 120);
+  Layer* fc4 = new FullyConnected(120, 84);
+  Layer* fc5 = new FullyConnected(84, 10);
+  Layer* relu1 = new ReLU;
+  Layer* relu2 = new ReLU;
+  Layer* relu3 = new ReLU;
+  Layer* relu4 = new ReLU;
   Layer* softmax = new Softmax;
+  dnn.add_layer(conv1);
+  dnn.add_layer(relu1);
   dnn.add_layer(pool1);
-  dnn.add_layer(fc2);
+  dnn.add_layer(conv2);
+  dnn.add_layer(relu2);
+  dnn.add_layer(pool2);
+  dnn.add_layer(fc3);
+  dnn.add_layer(relu3);
+  dnn.add_layer(fc4);
+  dnn.add_layer(relu4);
+  dnn.add_layer(fc5);
   dnn.add_layer(softmax);
 
-  // load parameters
-  std::vector<float> fc2Parameters = loadParametersFromFile("../parameters/tt.txt");
-  std::cout << "fc2 paramters: " << fc2Parameters.size() << std::endl;
-  fc2->set_parameters(fc2Parameters);
+  if (isTraining) {
+    // loss
+    Loss* loss = new CrossEntropy;
+    dnn.add_loss(loss);
+    // train & test
+    SGD opt(0.001, 5e-4, 0.9, true);
+    // SGD opt(0.001);
+    const int n_epoch = 4;
+    const int batch_size = 128;
+    for (int epoch = 0; epoch < n_epoch; epoch ++) {
+      shuffle_data(dataset.train_data, dataset.train_labels);
+      for (int start_idx = 0; start_idx < n_train; start_idx += batch_size) {
+        int ith_batch = start_idx / batch_size;
+        Matrix x_batch = dataset.train_data.block(0, start_idx, dim_in,
+                                      std::min(batch_size, n_train - start_idx));
+        Matrix label_batch = dataset.train_labels.block(0, start_idx, 1,
+                                      std::min(batch_size, n_train - start_idx));
+        Matrix target_batch = one_hot_encode(label_batch, 10);
+        if (false && ith_batch % 10 == 1) {
+          std::cout << ith_batch << "-th grad: " << std::endl;
+          dnn.check_gradient(x_batch, target_batch, 10);
+        }
+        dnn.forward(x_batch);
+        dnn.backward(x_batch, target_batch);
+        // display
+        if (ith_batch % 50 == 0) {
+          std::cout << ith_batch << "-th batch, loss: " << dnn.get_loss()
+          << std::endl;
+        }
+        // optimize
+        dnn.update(opt);
+      }
+      // test
+      dnn.forward(dataset.test_data);
+      float acc = compute_accuracy(dnn.output(), dataset.test_labels);
+      std::cout << std::endl;
+      std::cout << epoch + 1 << "-th epoch, test acc: " << acc << std::endl;
+      std::cout << std::endl;
+    }
+  } else {
+    // load parameters
+    std::vector<float> conv1Parameters = loadParametersFromFile("../parameters/conv1.txt");
+    std::vector<float> conv2Parameters = loadParametersFromFile("../parameters/conv2.txt");
+    std::vector<float> fc3Parameters = loadParametersFromFile("../parameters/fc3.txt");
+    std::vector<float> fc4Parameters = loadParametersFromFile("../parameters/fc4.txt");
+    std::vector<float> fc5Parameters = loadParametersFromFile("../parameters/fc5.txt");
+    std::cout << "conv1 paramters: " << conv1Parameters.size() << std::endl;
+    conv1->set_parameters(conv1Parameters);
+    std::cout << "conv2 paramters: " << conv2Parameters.size() << std::endl;
+    conv2->set_parameters(conv2Parameters);
+    std::cout << "fc3 paramters: " << fc3Parameters.size() << std::endl;
+    fc3->set_parameters(fc3Parameters);
+    std::cout << "fc4 paramters: " << fc4Parameters.size() << std::endl;
+    fc4->set_parameters(fc4Parameters);
+    std::cout << "fc5 paramters: " << fc5Parameters.size() << std::endl;
+    fc5->set_parameters(fc5Parameters);
 
-  // Test (Run forward)
-  dnn.forward(dataset.test_data);
-  float acc = compute_accuracy(dnn.output(), dataset.test_labels);
-  std::cout << std::endl;
-  std::cout << "Test acc: " << acc << std::endl;
+    // Test (Run forward)
+    dnn.forward(dataset.test_data);
+    float acc = compute_accuracy(dnn.output(), dataset.test_labels);
+    std::cout << std::endl;
+    std::cout << "Test acc: " << acc << std::endl;
+  }
 
   return 0;
 }
