abac37ffa47e4ff9640e9f09c3707c2e4cb95978
loc4atnt <dorakid153@gmail.com>
2023-12-25 20:52:11 +0700

    moe

--- src/device/Operator.cu
+++ src/device/Operator.cu
@@ -1,1 +1,18 @@
 #include "Operator.h"
+
+// index = c*n_row + r
+
+// A = (n, m)   B = (m, l)
+void dev_matrixMul(float *res, float *A, float *B, int n, int m, int l) {
+
+}
+
+// des = (n, m) vec = (n)
+void dev_matrixColwiseAddVec(float *des, float *vec, int n, int m) {
+
+}
+
+// des = (n, m) vec = (m)
+void dev_matrixRowwiseAddVec(float *des, float *vec, int n, int m) {
+  
+}
--- src/device/Operator.h
+++ src/device/Operator.h
@@ -4,8 +4,15 @@
 #include <cuda_runtime.h>
 #include "Util.h"
 
-// Matrix matrixMul(const Matrix &A, const Matrix &B, bool usingDevice=false);
-// void matrixColwiseAddVec(const Matrix &des, const Vector &vec, bool usingDevice=false);
-// void matrixRowwiseAddVec(const Matrix &des, const Vector &vec, bool usingDevice=false);
+// index = c*n_row + r
+
+// A = (n, m)   B = (m, l)
+void dev_matrixMul(float *res, float *A, float *B, int n, int m, int l);
+
+// des = (n, m) vec = (n)
+void dev_matrixColwiseAddVec(float *des, float *vec, int n, int m);
+
+// des = (n, m) vec = (m)
+void dev_matrixRowwiseAddVec(float *des, float *vec, int n, int m);
 
 #endif
