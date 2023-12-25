065d9843f8848d0356a6c2bd79ef48e47d35977f
loc4atnt <dorakid153@gmail.com>
2023-12-25 17:38:07 +0700

    moe

--- main.cc
+++ main.cc
@@ -32,11 +32,11 @@ int main(int argc, char *argv[]) {
   Matrix m;
   m.resize(2, 3);
   m(0, 0) = 1;
-  m(1, 0) = 2;
-  m(2, 0) = 3;
-  // m(1, 0) = 4;
-  // m(1, 1) = 5;
-  // m(1, 2) = 6;
+  m(0, 1) = 2;
+  m(0, 2) = 3;
+  m(1, 0) = 4;
+  m(1, 1) = 5;
+  m(1, 2) = 6;
   for (int i = 0; i < 6; i++) {
     std::cout << m.data()[i] << std::endl;
   }
