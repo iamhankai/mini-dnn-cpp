073a4ba7afd6dba7584ef7b61bbe8ef1f85dabb7
loc4atnt <dorakid153@gmail.com>
2023-12-25 21:17:36 +0700

    moe

--- src/operator/Operator.cpp
+++ src/operator/Operator.cpp
@@ -4,7 +4,7 @@ Matrix matrixMul(const Matrix &A, const Matrix &B, bool usingDevice) {
   if (usingDevice) {
     Matrix result;
     result.resize(A.rows(), B.cols());
-    dev_matrixMul(result.data(), A.data(), B.data(), A.rows(), A.cols(), B.cols());
+    dev_matrixMul((float*)result.data(), (float*)A.data(), (float*)B.data(), A.rows(), A.cols(), B.cols());
     return result;
   } else {
     return A * B;
@@ -13,7 +13,7 @@ Matrix matrixMul(const Matrix &A, const Matrix &B, bool usingDevice) {
 
 void matrixColwiseAddVec(Matrix &des, const Vector &vec, bool usingDevice) {
   if (usingDevice) {
-    dev_matrixColwiseAddVec(des.data(), vec.data(), des.rows(), des.cols());
+    dev_matrixColwiseAddVec((float*)des.data(), (float*)vec.data(), des.rows(), des.cols());
   } else {
     des.colwise() += vec;
   }
@@ -21,7 +21,7 @@ void matrixColwiseAddVec(Matrix &des, const Vector &vec, bool usingDevice) {
 
 void matrixRowwiseAddVec(Matrix &des, const Vector &vec, bool usingDevice) {
   if (usingDevice) {
-    dev_matrixRowwiseAddVec(des.data(), vec.data(), des.rows(), des.cols());
+    dev_matrixRowwiseAddVec((float*)des.data(), (float*)vec.data(), des.rows(), des.cols());
   } else {
     des.rowwise() += vec.transpose();
   }
